febc74fe378719e3af1a18273d45bc171579709c
Mimifan <mimifan.dev@gmail.com>
2023-03-29 13:59:27 +0200

    Initial Commit adding Main.java

---
+++ src/fr/mimifan/tetrisia/Main.java
@@ -0,0 +1,30 @@
+package fr.mimifan.tetrisia;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Main {
+	
+	private static int hauteur=700,largeur=400;
+	
+	public static int score = 0;
+	public static JLabel scoreLabel = new JLabel("Score: " + score);
+	
+	public static JFrame frame = new JFrame("TetrisIA");
+	public static JPanel mainPanel = new JPanel(new BorderLayout());
+    
+	public static void main(String[] args) {
+		frame.setSize(largeur,hauteur);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	
+
+}
